33fb81e04f49bedad6c946f9cfc03efdbda6f863
Kyaroline Rodrigues <blossomblue7777.com>
2026-07-13 15:54:41 +0530

    Improve skill extraction using regex

--- backend/app/skill_extractor.py
+++ backend/app/skill_extractor.py
@@ -1,3 +1,5 @@
+import re
+
 SKILLS = [
     "python",
     "java",
@@ -39,7 +41,9 @@ def extract_skills(text):
     found_skills = []
 
     for skill in SKILLS:
-        if skill in text:
+        pattern = r"\b" + re.escape(skill.lower()) + r"\b"
+
+        if re.search(pattern, text):
             found_skills.append(skill)
 
     return sorted(list(set(found_skills)))
